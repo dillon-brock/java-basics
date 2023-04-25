f62d959d62c2df1bd72d16b8e16d829d6c497ac3
Dillon Brock <mdillon1297@gmail.com>
2023-04-25 14:44:20 -0700

    added Packages (basics on packages)

---
+++ Packages/src/com/dbtest/Main.java
@@ -0,0 +1,12 @@
+package com.dbtest;
+
+import com.dbtest.first.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Item firstItem = new Item("Burger");
+        System.out.println(firstItem);
+    }
+}
